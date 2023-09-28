5ade2f1223877093825ca49bda4ce7397d42635c
amy0h <amy0h@gmail.com>
2023-09-29 02:55:33 +1300

    Added style and enhanced responsiveness with CSS

--- src/App.jsx
+++ src/App.jsx
@@ -12,8 +12,7 @@ const chartData = {
     {
       label: 'Amount',
       data: dataValues,
-      backgroundColor: 'rgb(236,117,93)',
-      hoverBackgroundColor: 'rgb(118,181,188)',
+      hoverBackgroundColor: 'rgb(255,155,134)',
       borderRadius: 3,
     },
   ],
@@ -21,29 +20,29 @@ const chartData = {
 
 function App() {
   return (
-    <>
+    <main>
       <div className='balance-container'>
         <div>
           <p className='balance-title'>My balance</p>
-          <p className='balance-amount'>$921.48</p>
+          <p className='balance-amount bold'>$921.48</p>
         </div>
         <img className='logo' src='./logo.svg' alt='logo' />
       </div>
       <div className='chart-container'>
-        <h1>Spending - Last 7 Days</h1>
+        <h1 className='chart-title bold'>Spending - Last 7 Days</h1>
         <BarChart data={chartData} /> {/* Render your BarChart component */}
         <div className='total-container'>
-          <div>
+          <div className='total-left'>
             <p className='total-title'>Total this month</p>
-            <p className='total-amount'>$921.48</p>
+            <p className='total-amount bold'>$921.48</p>
           </div>
-          <div>
-            <p className='total-percentage'>+2.4%</p>
+          <div className='total-right'>
+            <p className='total-percentage bold'>+2.4%</p>
             <p className='total-message'>from last month</p>
           </div>
         </div>
       </div>
-    </>
+    </main>
   );
 }
 
--- src/components/BarChart.jsx
+++ src/components/BarChart.jsx
@@ -1,51 +1,75 @@
 import React from 'react';
 import { Bar } from 'react-chartjs-2';
-import {
-  Chart as ChartJS,
-  CategoryScale,
-  LinearScale,
-  BarElement,
-  Title,
-  Tooltip,
-  Legend,
-} from 'chart.js';
+import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend, defaults } from 'chart.js';
 
-ChartJS.register(
-  CategoryScale,
-  LinearScale,
-  BarElement,
-  Title,
-  Tooltip,
-  Legend
-);
+ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
+
+defaults.font.family = 'Helvetica Neue';
+defaults.font.size = 15;
 
 export const BarChart = ({ data }) => {
+  // Find the index of the maximum value in dataValues array
+  const maxIndex = data.datasets[0].data.indexOf(Math.max(...data.datasets[0].data));
+
+  // Create an array of background colors with a different color for the max bar
+  const backgroundColor = data.labels.map((_, index) => (index === maxIndex ? 'rgb(118,181,188)' : 'rgb(236, 117, 93)'));
+
+  // Modify the data object to set backgroundColor based on the index
+  data.datasets[0].backgroundColor = backgroundColor;
+
   const chartOptions = {
     responsive: true,
     plugins: {
       legend: {
-        position: 'top',
+        display: false, 
       },
-      title: {
-        display: true,
-        text: 'Spending - Last 7 days',
+      tooltip: {
+        yAlign: 'bottom',
+        
+        backgroundColor: 'rgba(0, 0, 0, 0.8)', 
+        borderColor: 'rgba(255, 255, 255, 0.5)', 
+        borderWidth: 1, 
+        displayColors: false, 
+        padding: 8, 
+        caretSize: 0,
+        callbacks: {
+          title: () => '',
+          label: (tooltipItem) => {
+            return `$${tooltipItem.formattedValue}`;
+          },
+        },
+        bodyFont: {
+          family: 'Helvetica Neue',
+          weight: 'bold',
+          size: 15,
+        }
       },
     },
     scales: {
       x: {
         grid: {
-          display: false, // Remove horizontal grid lines
+          display: false,
         },
       },
       y: {
+        display: false, 
+        title: {
+          display: false, 
+        },
         grid: {
-          display: false, // Remove vertical grid lines
+          display: false,
         },
       },
     },
   };
 
-  return <Bar data={data} options={chartOptions} />;
+  return (
+    <Bar
+      data={data}
+      options={chartOptions}
+      aria-label="Bar Chart" // Add an aria-label for accessibility
+    />
+  );
 };
 
 export default BarChart;
